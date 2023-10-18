019553e64c2ee7ec0b4c6dbee6b62267faffe3e8
asyed8 <asyed8@mail.bradley.edu>
2023-10-18 15:11:55 -0500

    second commit

---
+++ Javascript.js
@@ -0,0 +1,11 @@
+function validateForm()
+ {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    if (name === "" || email === "" || message === "") {
+        alert("All fields are required");
+        return false;
+
+    }
+}
